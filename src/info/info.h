e68c05c3b9085286e61010462410d0972591e2ac
NikLeberg <ruben@pg-ruben.ch>
2020-02-04 16:25:46 +0100

    wirking info leds

--- src/info/info.h
+++ src/info/info.h
@@ -51,7 +51,9 @@ struct info_input_t {
 
 struct info_ws_led_t {
     union {
-        uint8_t g, r, b;
+        struct {
+            uint8_t g, r, b;
+        };
         uint8_t grb[3];
     };
 };
@@ -91,9 +93,7 @@ bool info_init(gpio_num_t di, uint8_t bnoAddr);
 void info_task(void* arg);
 
 // ToDo
-static bool info_ws_tx(uint32_t color, bool final);
 static bool info_ws_update();
-static inline uint32_t info_ws_getColor(uint8_t ledNum, uint8_t color);
 static inline void info_ws_setRGB(uint8_t ledNum, uint8_t red, uint8_t green, uint8_t blue);
 static bool info_ws_init(gpio_num_t dataOut);
 static void IRAM_ATTR info_ws_interrupt(void* arg);
@@ -108,27 +108,8 @@ bool info_init(gpio_num_t dataOut, uint8_t addr) {
     // info_display_init();
     // I2S initialisieren
     if (info_ws_init(dataOut)) return true;
-    // i2s_config_t i2s_config = {
-    //     .mode = ((i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX)),
-    //     .sample_rate = WS_SAMPLE_RATE,
-    //     .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    //     .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-    //     .communication_format = ((i2s_comm_format_t) (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB)),
-    //     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    //     .dma_buf_count = 2,
-    //     .dma_buf_len = 128,
-    //     .use_apll = false
-    // };
-    // i2s_pin_config_t pin_config = {
-    //     .bck_io_num = GPIO_NUM_23,
-    //     .ws_io_num = I2S_PIN_NO_CHANGE,
-    //     .data_out_num = dataOut,
-    //     .data_in_num = I2S_PIN_NO_CHANGE
-    // };
-    // i2s_driver_install(WS_I2S, &i2s_config, 0, NULL);
-    //i2s_set_pin(WS_I2S, &pin_config);
     // installiere task
-    if (xTaskCreate(&info_task, "info", 2 * 1024, NULL, xInfo_PRIORITY, &xInfo_handle) != pdTRUE) return true;
+    if (xTaskCreate(&info_task, "info", 3 * 1024, NULL, xInfo_PRIORITY, &xInfo_handle) != pdTRUE) return true;
     return false;
 }
 
@@ -137,70 +118,35 @@ bool info_init(gpio_num_t dataOut, uint8_t addr) {
 void info_task(void* arg) {
     // Variablen
     struct info_input_t input;
-    uint32_t data0 = 0b10101110001110001110001110001110;
-    uint32_t data1 = 0b10100001110001110001110001110000;
+    uint8_t i = 0, j = 0;
     // Loop
+    for (uint8_t i = 0; i < WS_LED_NUM; ++i) {
+        info_ws_setRGB(i, 126, 0, 0);
+    }
     while (true) {
-        vTaskDelay(1000);
-        //info_ws_update();
-        //continue;
-        // DEBUG
-        ESP_LOGV("info", "pre");
-        ESP_LOGV("info", "I2S_CONF_REG          %#x", I2S[WS_I2S]->conf.val);
-        ESP_LOGV("info", "I2S_CONF1_REG         %#x", I2S[WS_I2S]->conf1.val);
-        ESP_LOGV("info", "I2S_CONF2_REG         %#x", I2S[WS_I2S]->conf2.val);
-        ESP_LOGV("info", "I2S_TIMING_REG        %#x", I2S[WS_I2S]->timing.val);
-        ESP_LOGV("info", "I2S_FIFO_CONF_REG     %#x", I2S[WS_I2S]->fifo_conf.val);
-        ESP_LOGV("info", "I2S_CONF_SINGLE_DATA_REG %#x", I2S[WS_I2S]->conf_single_data);
-        ESP_LOGV("info", "I2S_CONF_CHAN_REG     %#x", I2S[WS_I2S]->conf_chan.val);
-        ESP_LOGV("info", "I2S_LC_HUNG_CONF_REG  %#x", I2S[WS_I2S]->lc_hung_conf.val);
-        ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-        ESP_LOGV("info", "I2S_SAMPLE_RATE_CONF_REG %#x", I2S[WS_I2S]->sample_rate_conf.val);
-        ESP_LOGV("info", "I2S_PD_CONF_REG       %#x", I2S[WS_I2S]->pd_conf.val);
-        ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-        ESP_LOGV("info", "I2S_STATE_REG I2S     %#x", I2S[WS_I2S]->state.val);
-        ESP_LOGV("info", "I2S_LC_CONF_REG I2S   %#x", I2S[WS_I2S]->lc_conf.val);
-        ESP_LOGV("info", "I2S_LC_STATE1_REG     %#x", I2S[WS_I2S]->lc_state1);
-        ESP_LOGV("info", "I2S_PDM_CONF_REG      %#x", I2S[WS_I2S]->pdm_conf.val);
-
-        ESP_LOGV("info", "%u-write %u fifo %u", 0, data0, I2S[WS_I2S]->reserved_0);
-        I2S[WS_I2S]->reserved_0 = data0;
-        ESP_LOGV("info", "%u-write %u fifo %u", 1, data1, I2S[WS_I2S]->reserved_0);
-        I2S[WS_I2S]->reserved_0 = data1;
-        // TX start
-        I2S[WS_I2S]->conf.tx_start = 1;
-        while (!I2S[WS_I2S]->state.tx_idle); // warte auf Ende
-        I2S[WS_I2S]->conf.tx_start = 0;
-
-        // DEBUG
-        ESP_LOGV("info", "apre");
-        ESP_LOGV("info", "I2S_CONF_REG          %#x", I2S[WS_I2S]->conf.val);
-        ESP_LOGV("info", "I2S_CONF1_REG         %#x", I2S[WS_I2S]->conf1.val);
-        ESP_LOGV("info", "I2S_CONF2_REG         %#x", I2S[WS_I2S]->conf2.val);
-        ESP_LOGV("info", "I2S_TIMING_REG        %#x", I2S[WS_I2S]->timing.val);
-        ESP_LOGV("info", "I2S_FIFO_CONF_REG     %#x", I2S[WS_I2S]->fifo_conf.val);
-        ESP_LOGV("info", "I2S_CONF_SINGLE_DATA_REG %#x", I2S[WS_I2S]->conf_single_data);
-        ESP_LOGV("info", "I2S_CONF_CHAN_REG     %#x", I2S[WS_I2S]->conf_chan.val);
-        ESP_LOGV("info", "I2S_LC_HUNG_CONF_REG  %#x", I2S[WS_I2S]->lc_hung_conf.val);
-        ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-        ESP_LOGV("info", "I2S_SAMPLE_RATE_CONF_REG %#x", I2S[WS_I2S]->sample_rate_conf.val);
-        ESP_LOGV("info", "I2S_PD_CONF_REG       %#x", I2S[WS_I2S]->pd_conf.val);
-        ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-        ESP_LOGV("info", "I2S_STATE_REG I2S     %#x", I2S[WS_I2S]->state.val);
-        ESP_LOGV("info", "I2S_LC_CONF_REG I2S   %#x", I2S[WS_I2S]->lc_conf.val);
-        ESP_LOGV("info", "I2S_LC_STATE1_REG     %#x", I2S[WS_I2S]->lc_state1);
-        ESP_LOGV("info", "I2S_PDM_CONF_REG      %#x", I2S[WS_I2S]->pdm_conf.val);
-        // Tx start
-        //I2S[WS_I2S]->conf.tx_start = 1;
-        //while (I2S[WS_I2S]->state.tx_idle); // warte auf Start
-        //while (!I2S[WS_I2S]->state.tx_idle); // warte auf Ende
-        //I2S[WS_I2S]->conf.tx_start = 0;
-        //ESP_LOGV("info", "TX done");
-        //I2S[WS_I2S]->conf.tx_start = 0;
-        //I2S[WS_I2S]->conf.tx_fifo_reset = 1;
-        //I2S[WS_I2S]->conf.tx_fifo_reset = 0;
+        vTaskDelay(1);
+        info_ws_update();
+        for (uint8_t i = 0; i < WS_LED_NUM; ++i) {
+            //info_ws_setRGB(i, (esp_random() % 256) / 2, 0, 0);
+            info_ws_setRGB(i, 0, 0, 0);
+        }
+        ++j;
+        if (j > WS_LED_NUM - 1) j = 0;
+        info_ws_setRGB(j, 0x1 << 1, 0, 0);
+        info_ws_setRGB((j + 1) % 24, 0x1 << 3, 0, 0);
+        info_ws_setRGB((j + 2) % 24, 0x1 << 5, 0, 0);
+        info_ws_setRGB((j + 3) % 24, 0x1 << 7, 0, 0);
+        info_ws_setRGB(j + 8, 0, 0x1 << 1, 0);
+        info_ws_setRGB((j + 8 + 1) % 24, 0, 0x1 << 3, 0);
+        info_ws_setRGB((j + 8 + 2) % 24, 0, 0x1 << 5, 0);
+        info_ws_setRGB((j + 8 + 3) % 24, 0, 0x1 << 7, 0);
+        info_ws_setRGB(j + 16, 0, 0, 0x1 << 1);
+        info_ws_setRGB((j + 16 + 1) % 24, 0, 0, 0x1 << 3);
+        info_ws_setRGB((j + 16 + 2) % 24, 0, 0, 0x1 << 5);
+        info_ws_setRGB((j + 16 + 3) % 24, 0, 0, 0x1 << 7);
         continue;
 
+        continue;
 
         if (xQueueReceive(xInfo_input, &input, 5000 / portTICK_PERIOD_MS) == pdTRUE) {
             switch (input.type) {
@@ -263,34 +209,13 @@ static bool info_ws_init(gpio_num_t dataOut) {
     I2S[WS_I2S]->timing.val = 0;
 
     // Master starten
-    //I2S[WS_I2S]->conf.tx_start = 1;
+    I2S[WS_I2S]->conf.tx_start = 1;
 
     // Test 1
     I2S[WS_I2S]->conf2.lcd_en = 0; // Paraleller LCD Modi deaktivieren
-    //I2S[WS_I2S]->conf1.tx_stop_en = 1; // TX = 0 mach FIFO leer, force deak, data_num = 0, master immer an
-
-    
-    // DEBUG
-    ESP_LOGV("info", "I2S_CONF_REG          %#x", I2S[WS_I2S]->conf.val);
-    ESP_LOGV("info", "I2S_CONF1_REG         %#x", I2S[WS_I2S]->conf1.val);
-    ESP_LOGV("info", "I2S_CONF2_REG         %#x", I2S[WS_I2S]->conf2.val);
-    ESP_LOGV("info", "I2S_TIMING_REG        %#x", I2S[WS_I2S]->timing.val);
-    ESP_LOGV("info", "I2S_FIFO_CONF_REG     %#x", I2S[WS_I2S]->fifo_conf.val);
-    ESP_LOGV("info", "I2S_CONF_SINGLE_DATA_REG %#x", I2S[WS_I2S]->conf_single_data);
-    ESP_LOGV("info", "I2S_CONF_CHAN_REG     %#x", I2S[WS_I2S]->conf_chan.val);
-    ESP_LOGV("info", "I2S_LC_HUNG_CONF_REG  %#x", I2S[WS_I2S]->lc_hung_conf.val);
-    ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-    ESP_LOGV("info", "I2S_SAMPLE_RATE_CONF_REG %#x", I2S[WS_I2S]->sample_rate_conf.val);
-    ESP_LOGV("info", "I2S_PD_CONF_REG       %#x", I2S[WS_I2S]->pd_conf.val);
-    ESP_LOGV("info", "I2S_CLKM_CONF_REG     %#x", I2S[WS_I2S]->clkm_conf.val);
-    ESP_LOGV("info", "I2S_STATE_REG I2S     %#x", I2S[WS_I2S]->state.val);
-    ESP_LOGV("info", "I2S_LC_CONF_REG I2S   %#x", I2S[WS_I2S]->lc_conf.val);
-    ESP_LOGV("info", "I2S_LC_STATE1_REG     %#x", I2S[WS_I2S]->lc_state1);
-    ESP_LOGV("info", "I2S_PDM_CONF_REG      %#x", I2S[WS_I2S]->pdm_conf.val);
     
     // Interrupt registrieren
-    //if (esp_intr_alloc(ETS_I2S0_INTR_SOURCE + WS_I2S, ESP_INTR_FLAG_IRAM, info_ws_interrupt, NULL, NULL)) return true;
-    //I2S[WS_I2S]->int_ena.tx_rempty = 1;
+    if (esp_intr_alloc(ETS_I2S0_INTR_SOURCE + WS_I2S, ESP_INTR_FLAG_IRAM, info_ws_interrupt, NULL, NULL)) return true;
 
     return false;
 }
@@ -302,79 +227,59 @@ static inline void info_ws_setRGB(uint8_t ledNum, uint8_t red, uint8_t green, ui
     return;
 }
 
-// "Upsampling" von 8 Bit RGB-Wert auf 24 Bit 0 -> 100, 1 -> 110
-static inline uint32_t info_ws_getColor(uint8_t ledNum, uint8_t grb) {
-    uint32_t color = 0;
-    for (uint8_t i = 0; i < 8; ++i) {
-        color = color << 3;
-        if ((info.leds[ledNum].grb[grb] >> (7 - i)) & 0x1) {
-            color |= 0b110;
-        } else {
-            color |= 0b100;
-        }
-    }
-    ESP_LOGV("info", "LED-%u, %c %u > %u", ledNum, (grb == 0) ? 'g' : (grb == 1) ? 'r' : 'b', info.leds[ledNum].grb[grb], color);
-    return color;
-}
-
 static bool info_ws_update() {
-    // FIFO Reset
-    I2S[WS_I2S]->conf.tx_fifo_reset = 1;
-    I2S[WS_I2S]->conf.tx_fifo_reset = 0;
-    // FIFO mit aktuellem Zustand füllen
-    uint32_t color = 0;
-    // for (uint8_t i = 0; i < 1; ++i) {
-    for (uint8_t i = 0; i < WS_LED_NUM; ++i) {
-        for (uint8_t j = 0; j < 3; ++j) {
-            //color = info_ws_getColor(i, j);
-            color = 0b00000000110110110110110110110110;
-            if (info_ws_tx(color, false)) return true;
+    // RGB-Farben in I2S-Bitstream wandeln
+    uint8_t grbStream[WS_LED_NUM * 3 * 3];
+    for (uint8_t i = 0; i < WS_LED_NUM; ++i) { // jede LED
+        for (uint8_t j = 0; j < 3; ++j) { // jede Farbe
+            uint32_t color = 0;
+            for (uint8_t k = 0; k < 8; ++k) { // jedes Farbbit
+                color <<= 3;
+                if ((info.leds[i].grb[j] >> (7 - k)) & 0x1) {
+                    color |= 0b110; // 1 -> 110
+                } else {
+                    color |= 0b100; // 0 -> 100
+                }
+            }
+            grbStream[(i * 9) + (j * 3)] = color >> 16;
+            grbStream[(i * 9) + (j * 3) + 1] = color >> 8;
+            grbStream[(i * 9) + (j * 3) + 2] = color;
         }
     }
-    if (info_ws_tx(0, true)) return true;
-    // Tx start
-    I2S[WS_I2S]->conf.tx_start = 1;
-    while (!I2S[WS_I2S]->state.tx_idle) {
-        ets_delay_us(1000);
+    // FIFO mit Bitstream füllen
+    uint16_t pos = 0;
+    uint32_t buffer = 0;
+    for (; pos < sizeof(grbStream); pos = pos + 4) {
+        I2S[WS_I2S]->reserved_0 = grbStream[pos] << 24 | grbStream[pos + 1] << 16 | grbStream[pos + 2] << 8 | grbStream[pos + 3];
     }
-    ESP_LOGV("info", "TX done");
-    I2S[WS_I2S]->conf.tx_start = 0;
-    return false;
-}
+    pos -= 4;
+    switch (sizeof(grbStream) - pos) {
+        default:
+        case (0):
+            break;
+        case (1):
+            I2S[WS_I2S]->reserved_0 = grbStream[pos] << 24;
+            break;
+        case (2):
+            I2S[WS_I2S]->reserved_0 = grbStream[pos] << 24 | grbStream[pos + 1] << 16;
+            break;
+        case (3):
+            I2S[WS_I2S]->reserved_0 = grbStream[pos] << 24 | grbStream[pos + 1] << 16 | grbStream[pos + 2] << 8;
+            break;
+    }
+    // TX start
+    I2S[WS_I2S]->int_ena.tx_rempty = 1;
 
-// schreibe die 3 Bytes an Farben in Zwischenspeicher, in FIFO schieben sobald 4 Bytes bereit
-static bool info_ws_tx(uint32_t color, bool final) {
-    static uint8_t position;
-    static uint64_t buffer;
-    if (I2S[WS_I2S]->int_st.tx_wfull) return true; // FIFO voll
-    ESP_LOGV("info", "TX %#llx", buffer);
-    buffer |= ((uint64_t) (color & 0x00ffffff)) << (8 * (5 - position));
-    ESP_LOGV("info", "TX %u %#x %#llx", position, (uint32_t) (color & 0x00ffffff), buffer);
-    // 0x00924924 - alles 0
-    // 0x0000000000924924, p:0
-    // 0x9249240000000000, p:3
-    // 0x9249249249240000, p:3
-    position += 3;
-    if (position >= 4) {
-        // schreibe in tx-fifo
-        ESP_LOGV("info", "FIFO %#x", (uint32_t) (buffer >> 32));
-        I2S[WS_I2S]->reserved_0 = (buffer >> 32);
-        buffer <<= 32;
-        position -= 4;
-    }
-    if (final) {
-        if (buffer & 0x00000000ffffffff) { // wenn Fragmente noch übrig
-            if (info_ws_tx(0, false)) return true;
-        }
-        position = 0;
-        buffer = 0;
-    }
     return false;
 }
 
 static void IRAM_ATTR info_ws_interrupt(void* arg) {
     if (I2S[WS_I2S]->int_st.tx_rempty) {
-        I2S[WS_I2S]->conf.tx_start = 0; // TX beenden
+        I2S[WS_I2S]->int_ena.tx_rempty = 0; // Interrupt deaktivieren
+        //I2S[WS_I2S]->conf.tx_start = 0; // TX beenden
+        for (uint32_t i = 0; i < 64; ++i) { // FIFO überschreiben damit nicht nochmal gesendet wird
+            I2S[WS_I2S]->reserved_0 = 0;
+        }
     }
-    I2S[WS_I2S]->int_clr.val = I2S[WS_I2S]->int_st.val; // Interrupts löschen 
+    I2S[WS_I2S]->int_clr.val = I2S[WS_I2S]->int_st.val; // Interrupts zurücksetzen 
 }
