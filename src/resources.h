45b80731cc129904bee72fbaca0e9b7097cfb922
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2020-02-12 00:15:03 +0100

    resource cleanup

--- src/resources.h
+++ src/resources.h
@@ -4,12 +4,13 @@
  * Author: Niklaus Leuenberger
  * Date:   2020-01-13
  * ----------------------------
- *   Globale Ressourcen- und Task-Handles zur Interprozesskommunikation.
- *   Handles müssen von den Modulen initialisiert werden.
+ * Globale Ressourcen- und Task-Handles zur Interprozesskommunikation.
+ * Handles müssen von den Modulen initialisiert werden.
  */
 
 
 /** Externe Abhängigkeiten **/
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -18,32 +19,32 @@
 #pragma once
 
 // Physikalische Ressourcen
-SemaphoreHandle_t sI2C = NULL;
+SemaphoreHandle_t sI2C;
 
 // Virtuelle Ressourcen
 /* keine */
 
 /** Sensorik **/
 // Main
-TaskHandle_t xSensors_handle = NULL;
+TaskHandle_t xSensors_handle;
 #define xSensors_PRIORITY (7U)
-QueueHandle_t xSensors_input = NULL;
+QueueHandle_t xSensors_input;
 // Ultrasonic
-TaskHandle_t xUlt_handle = NULL;
-QueueHandle_t xUlt_input = NULL;
+TaskHandle_t xUlt_handle;
+QueueHandle_t xUlt_input;
 // GPS
-TaskHandle_t xGps_handle = NULL;
-QueueHandle_t xGps_input = NULL;
+TaskHandle_t xGps_handle;
+QueueHandle_t xGps_input;
 // BNO
-TaskHandle_t xBno_handle = NULL;
-QueueHandle_t xBno_input = NULL;
+TaskHandle_t xBno_handle;
+QueueHandle_t xBno_input;
 
 // Remote
-TaskHandle_t xRemote_handle = NULL;
+TaskHandle_t xRemote_handle;
 #define xRemote_PRIORITY (6U)
-QueueHandle_t xRemote_input = NULL;
+QueueHandle_t xRemote_input;
 
 // Info
-TaskHandle_t xInfo_handle = NULL;
+TaskHandle_t xInfo_handle;
 #define xInfo_PRIORITY (1U)
-QueueHandle_t xInfo_input = NULL;
+QueueHandle_t xInfo_input;
