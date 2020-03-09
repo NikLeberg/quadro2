67b1955d1bcabe957c3555ac27fc326352524355
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2020-03-09 21:28:05 +0100

    remove settings

--- src/remote/remote.h
+++ src/remote/remote.h
@@ -43,19 +43,3 @@
  * returns: false -> Erfolg, true -> Error
  */
 bool remote_init(char* ssid, char* pw);
-
-/*
- * Function: remote_settingRegister
- * ----------------------------
- * Registriert eine Einstellung und speichert diese persistent in NVS.
- * Vorgenommene Veränderungen werden dem Callback mitgeteilt.
- * 
- * const char* taskTag: Taskname dem die Einstellung gehört
- * const char* settingTag: Name der Einstellung
- * remote_settingUpdate_t *updateCallback: Callback der über Veränderungen informiert wird
- * void *updateCallbackCookie: Cookie das dem Callback gegeben wird, bsp. zur Unterscheidung der Einstellungen in einem Switch
- * struct setting_t *setting: zu verwaltende Einstellung
- *
- * returns: Handle der Einstellung bei Erfolg, sonst NULL
- */
-bool remote_settingRegister(const char* taskTag, const char* settingTag, remote_settingUpdate_t *updateCallback, void *updateCallbackCookie, struct setting_t *setting);
