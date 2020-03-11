5c451463ac92ff75da80109e5ec3d0bdb9201d27
NikLeberg <ruben@pg-ruben.ch>
2020-03-11 16:55:18 +0100

    pv expanded & parameters added

--- src/intercom.h
+++ src/intercom.h
@@ -58,16 +58,18 @@ typedef struct {
 } event_t;
 
 typedef enum {
-    VALUE_TYPE_NONE,
+    VALUE_TYPE_NONE,    // nur Event
     VALUE_TYPE_UINT,
     VALUE_TYPE_INT,
-    VALUE_TYPE_FLOAT
+    VALUE_TYPE_FLOAT,
+    VALUE_TYPE_POINTER  // Pointer zu einer Variabel abnormalen Typs
 } value_type_t;
 
 typedef union {
     uint32_t ui;
     int32_t i;
     float f;
+    void *p;
 } value_t;
 
 
@@ -161,14 +163,50 @@ typedef struct {
 
 #define PARAMETER(name, address, type)  {(name), (void *)(address), (type)}
 
+typedef struct parameter_list_s {
+    const char *task;
+    QueueHandle_t owner;
+    parameter_t *parameters;
+    size_t length;
+    struct parameter_list_s *next;
+} parameter_list_t;
+
+#define PARAMETER_LIST(task, parameters, length)    parameter_list_t parameters##_list = {task, NULL, parameters, length, NULL};
+
+#define parameterSet(owner, parameterNum, value)    intercom_parameterSet(owner, parameterNum, (void*)&value)
+
+#define parameterGet(owner, parameterNum, variable) intercom_parameterGet(owner, parameterNum, (void*)&variable)
+
+bool intercom_parameterSet(QueueHandle_t owner, uint32_t parameterNum, void *value);
+bool intercom_parameterGet(QueueHandle_t owner, uint32_t parameterNum, void *variable);
+
 
 /*
- * Types: Prozessvariabel
+ * PV - Prozessvariabel
  * ----------------------------
- * Variabeltypen zur Definition von Pvs
+ * Kommunikation per Publish / Subscribe.
+ * 1. Publisher-Task stellt Events / Prozessvariabeln zur verfügung:
+ *      typedef enum {
+ *          SENSORS_PV_X = 0,
+ *          SENSORS_PV_Y,
+ *          SENSORS_PV_Z,
+ *          SENSORS_PV_MAX
+ *      } sensors_pv_t;
+ *      pv_t sensors_pvs[SENSORS_PV_MAX] = {
+ *          PV("x", VALUE_TYPE_FLOAT),
+ *          PV("y", VALUE_TYPE_FLOAT),
+ *          PV("z", VALUE_TYPE_FLOAT),
+ *      };
+ *      PV_LIST("sensors", sensors_pvs, SENSORS_PV_MAX);
+ *      pvRegister(xSensors, sensors_pvs);
  * 
- * Intern ansprechbar per Enumerator resp. pv_t[x].
- * Extern ansprechbar per String.
+ * 2. Subscriber-Task registriert sich für Updates:
+ *      pv_t *subscription = intercom_pvSubscribe(xRemote, xSensors, SENSORS_PV_X);
+ * 
+ * 2. Publisher publiziert neue Werte an Intercom welche diese in die
+ *    Empfangsqueues der registrierten Subscriber sendet:
+ *      pvPublishFloat(xSensors, SENSORS_PV_X, 1.0f);
+ *      float x = pvGetFloat(subscription);
  */
 
 #define INTERCOM_PV_MAX_SUBSCRIBERS 3
@@ -199,21 +237,25 @@ typedef struct pv_list_s {
 #define pvPublishInt(publisher, pvNum, value)   intercom_pvPublish(publisher, pvNum, (value_t){.i = value})
 #define pvPublishFloat(publisher, pvNum, value) intercom_pvPublish(publisher, pvNum, (value_t){.f = value})
 
+#define pvGet(pv)           (pv->type == VALUE_TYPE_NONE ? true : false)
+#define pvGetUint(pv)       (pv->type == VALUE_TYPE_UINT ? pv->value.ui : 0UL)
+#define pvGetInt(pv)        (pv->type == VALUE_TYPE_INT ? pv->value.i : 0L)
+#define pvGetFloat(pv)      (pv->type == VALUE_TYPE_FLOAT ? pv->value.f : 0.0f)
+#define pvGetPointer(pv)    (pv->type == VALUE_TYPE_POINTER ? pv->value.p : NULL)
+
+void intercom_pvRegister(QueueHandle_t publisher, pv_list_t *list);
+pv_t *intercom_pvSubscribe(QueueHandle_t subscriber, QueueHandle_t publisher, uint32_t pvNum);
+void intercom_pvPublish(QueueHandle_t publisher, uint32_t pvNum, value_t value);
+
 
 /** To be made Private **/
 
 static struct {
     command_list_t *commandHead;
     setting_list_t *settingHead;
+    parameter_list_t *parameterHead;
     pv_list_t *pvHead;
 } intercom;
-
-bool intercom_init() {
-    if (nvs_flash_init()) {
-        if (nvs_flash_erase() || nvs_flash_init()) return true;
-    }
-    return false;
-}
 
 void intercom_commandRegister(QueueHandle_t owner, command_list_t *list) {
     // an Linked-List anhängen
@@ -250,14 +292,82 @@ void intercom_settingRegister(QueueHandle_t owner, setting_list_t *list) {
     last->next = list;
     // Einstellung aus NVS laden
     nvs_handle nvs;
-    if (nvs_open(list->task, NVS_READWRITE, &nvs)) return;
+    esp_err_t err;
+    err = nvs_open(list->task, NVS_READWRITE, &nvs);
+    if (err == ESP_ERR_NVS_NOT_INITIALIZED) {
+        if (nvs_flash_init()) {
+            if (nvs_flash_erase() || nvs_flash_init()) return;
+        }
+        nvs_open(list->task, NVS_READWRITE, &nvs);
+    } else if (err) return;
     for (size_t i = 0; i < list->length; ++i) {
-        esp_err_t ret = nvs_get_u32(nvs, list->settings[i].name, list->settings[i].address);
-        if (ret == ESP_ERR_NVS_NOT_FOUND) {
+        err = nvs_get_u32(nvs, list->settings[i].name, list->settings[i].address);
+        if (err == ESP_ERR_NVS_NOT_FOUND) {
             if (nvs_set_u32(nvs, list->settings[i].name, *(uint32_t*)(list->settings[i].address))) return;
             if (nvs_commit(nvs)) return;
-        } else if (ret) return;
+        } else if (err) return;
     }
+}
+
+void intercom_parameterRegister(QueueHandle_t owner, parameter_list_t *list) {
+    // an Linked-List anhängen
+    list->owner = owner;
+    parameter_list_t *last = intercom.parameterHead;
+    while (last) {
+        last = last->next;
+    }
+    last->next = list;
+}
+
+static parameter_t *intercom_parameterSearch(QueueHandle_t owner, uint32_t parameterNum) {
+    // Owner suchen
+    parameter_list_t *node = intercom.parameterHead;
+    while (true) {
+        if (!node) return NULL; // Owner nicht vorhanden
+        if (node->owner != owner) break;
+        node = node->next;
+    }
+    // Prüfen
+    if (node->length < parameterNum) return NULL; // Parameter nicht vorhanden
+    return &node->parameters[parameterNum];
+}
+
+bool intercom_parameterSet(QueueHandle_t owner, uint32_t parameterNum, void *value) {
+    parameter_t *parameter = intercom_parameterSearch(owner, parameterNum);
+    if (!parameter) return true;
+    switch (parameter->type) {
+        case (VALUE_TYPE_UINT):
+            *(uint32_t*)parameter->address = *(uint32_t*)value;
+            break;
+        case (VALUE_TYPE_INT):
+            *(int32_t*)parameter->address = *(int32_t*)value;
+            break;
+        case (VALUE_TYPE_FLOAT):
+            *(float*)parameter->address = *(float*)value;
+            break;
+        default:
+            break;
+    }
+    return false;
+}
+
+bool intercom_parameterGet(QueueHandle_t owner, uint32_t parameterNum, void *variable) {
+    parameter_t *parameter = intercom_parameterSearch(owner, parameterNum);
+    if (!parameter) return true;
+    switch (parameter->type) {
+        case (VALUE_TYPE_UINT):
+            *(uint32_t*)variable = *(uint32_t*)parameter->address;
+            break;
+        case (VALUE_TYPE_INT):
+            *(int32_t*)variable = *(int32_t*)parameter->address;
+            break;
+        case (VALUE_TYPE_FLOAT):
+            *(float*)variable = *(float*)parameter->address;
+            break;
+        default:
+            break;
+    }
+    return false;
 }
 
 void intercom_pvRegister(QueueHandle_t publisher, pv_list_t *list) {
@@ -270,7 +380,7 @@ void intercom_pvRegister(QueueHandle_t publisher, pv_list_t *list) {
     last->next = list;
 }
 
-static pv_t* intercom_pvGet(QueueHandle_t publisher, uint32_t pvNum) {
+static pv_t *intercom_pvSearch(QueueHandle_t publisher, uint32_t pvNum) {
     // Publisher suchen
     pv_list_t *node = intercom.pvHead;
     while (true) {
@@ -283,8 +393,8 @@ static pv_t* intercom_pvGet(QueueHandle_t publisher, uint32_t pvNum) {
     return &node->pvs[pvNum];
 }
 
-pv_t* intercom_pvSubscribe(QueueHandle_t subscriber, QueueHandle_t publisher, uint32_t pvNum) {
-    pv_t *pv = intercom_pvGet(publisher, pvNum);
+pv_t *intercom_pvSubscribe(QueueHandle_t subscriber, QueueHandle_t publisher, uint32_t pvNum) {
+    pv_t *pv = intercom_pvSearch(publisher, pvNum);
     if (!pv) return NULL;
     // Subscriber speichern
     for (uint8_t i = 0; i <= INTERCOM_PV_MAX_SUBSCRIBERS; ++i) {
@@ -296,7 +406,7 @@ pv_t* intercom_pvSubscribe(QueueHandle_t subscriber, QueueHandle_t publisher, ui
 }
 
 void intercom_pvPublish(QueueHandle_t publisher, uint32_t pvNum, value_t value) {
-    pv_t *pv = intercom_pvGet(publisher, pvNum);
+    pv_t *pv = intercom_pvSearch(publisher, pvNum);
     if (!pv) return;
     pv->value = value;
     // an alle Subscriber senden
