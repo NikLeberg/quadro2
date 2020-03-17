d6827f1ae22df851046e6424535b6cd476b43409
NikLeberg <ruben@pg-ruben.ch>
2020-03-17 16:05:47 +0100

    add linux command

--- gdbLine.cmd
+++ gdbLine.cmd
@@ -2,6 +2,7 @@
 :start
 set /p address=Adresse: 
 C:\Users\Nik\.platformio\packages\toolchain-xtensa32\bin\xtensa-esp32-elf-gdb "%cd%\.pio\build\nodemcu-32s\firmware.elf" -silent -ex "l *%address%" -ex "q"
+REM /home/ruben/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-gdb .pio/build/nodemcu-32s/firmware.elf
 goto start
 pause
 exit
