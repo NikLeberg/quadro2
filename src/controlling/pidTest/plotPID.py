8a61cdad14c4d1cb9a0b8d7d65f950bd4ccfc980
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2020-05-04 18:53:06 +0200

    track armed state

--- src/controlling/pidTest/plotPID.py
+++ src/controlling/pidTest/plotPID.py
@@ -10,6 +10,8 @@ def main():
     soll = [0.0]
     out = [0.0]
     throttle = [0.0]
+    armed = [0.0]
+    K = [0.0]
     with open(str(sys.argv[1])) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=';')
         rows = iter(readCSV)
@@ -19,6 +21,7 @@ def main():
         headSoll = row[2]
         headOut = row[3]
         headThrottle = row[4]
+        headArmed = row[5]
         row = next(rows)
         lastTimestamp = float(row[0])
         # über Zeilen iterieren
@@ -34,25 +37,45 @@ def main():
                     soll.append(soll[-1])
                     out.append(out[-1])
                     throttle.append(throttle[-1])
+                    armed.append(armed[-1])
+                    K.append(K[-1])
                 if (len(row) > 2 and row[2] != ''):
                     ist.append(ist[-1])
                     soll.append(float(row[2]))
                     out.append(out[-1])
                     throttle.append(throttle[-1])
+                    armed.append(armed[-1])
+                    K.append(K[-1])
                 if (len(row) > 3 and row[3] != ''):
                     ist.append(ist[-1])
                     soll.append(soll[-1])
                     out.append(float(row[3]))
                     throttle.append(throttle[-1])
+                    armed.append(armed[-1])
+                    if (armed[-1] > 0.0):
+                        K.append((K[-1] + (ist[-1] - soll[-1]) / out[-1]) / 2.0)
+                        print("K:", K[-1], "Ist:", ist[-1], "Soll:", soll[-1], "Out:", out[-1])
+                    else:
+                        K.append(0.0)
                 if (len(row) > 4 and row[4] != ''):
                     ist.append(ist[-1])
                     soll.append(soll[-1])
                     out.append(out[-1])
                     throttle.append(float(row[4]))
+                    armed.append(armed[-1])
+                    K.append(K[-1])
+                if (len(row) > 5 and row[5] != ''):
+                    ist.append(ist[-1])
+                    soll.append(soll[-1])
+                    out.append(out[-1])
+                    throttle.append(throttle[-1])
+                    armed.append(float(row[5]))
+                    if (armed[-1] > 0.0): K.append(K[-1])
+                    else: K.append(0.0)
                 # speichere für Plot
                 time.append(time[-1] + dT)
 
-            except (StopIteration) as e: # np.linalg.LinAlgError
+            except (StopIteration) as e:
                 print(e)
                 break
 
@@ -62,6 +85,8 @@ def main():
         plt.plot(time, soll, label=headSoll)
         plt.plot(time, out, label=headOut)
         plt.plot(time, throttle, label=headThrottle)
+        plt.plot(time, armed, label=headArmed)
+        # plt.plot(time, K, label="K")
         plt.legend()
         plt.show()
 
